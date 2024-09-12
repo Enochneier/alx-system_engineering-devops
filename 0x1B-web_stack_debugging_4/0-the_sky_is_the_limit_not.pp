a54e8dc36a6158f2374f0f81927241ad3cc44a09
Enochneier <enochneier@gmail.com>
2024-09-12 04:51:14 -0700

    0. Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+nx requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
+}
+
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
+}
